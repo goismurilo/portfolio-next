d41d700950ad211d53ad55ab04e0987642608b82
Murilo Gois <murilogois59@gmail.com>
2022-03-25 03:22:04 -0300

    build(Tailwind): Add config
    Color, FontFamily and Image background

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#262254",
+        "primary-medium": "#543884",
+        "primary-light": "#9A77CF",
+        "second-dark": "#A13670",
+      },
+      fontFamily: {
+        sans: ["Roboto", "Inter var", "sans-serif"],
+      },
+      backgroundImage: {
+        'background-landing': "url('../src/images/background-landing.png')",
+      }
+    },
   },
   plugins: [],
+
 }
