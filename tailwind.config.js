3a4a411c5fbe832ecb5b2177326c0462c7814910
Murilo Gois <murilogois59@gmail.com>
2023-12-23 17:27:58 -0300

    style(Theme): Add new colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,17 +12,34 @@ module.exports = {
         "second-dark": "#A13670",
         "second-medium": "#EC4176",
         "second-light": "#FFA45E",
+
+        //
+        // "primary-color-500": "#8A42DB",
+        // "primary-color-400": "#9b67e8",
+        "primary-color-500": "#262254",
+        "primary-color-400": "#543884",
+        "secondary-color": "#7DFFAF",
+
+        // Surface-colors
+        "surface-background": "#171023",
+        "surface-primary": "#2C243B",
+        "surface-secondary": "#413A4F",
+        "surface-tertiary": "#4E4563",
+
+        // Text-colors
+        "text-primary": "#F5F6F6",
+        "text-secondary": "#CDD0D4",
       },
       fontFamily: {
-        sans: ["Roboto", "Inter var", "sans-serif"],
+        sans: ["Hebbo", "Inter var", "sans-serif"],
+        cursive: ["Kalam", "cursive"],
       },
       backgroundImage: {
-        'background-landing': "url('../src/images/background-landing.png')",
-        'logo': "url('../src/images/logo-only.png')",
-        'logo-name': "url('../src/images/logo-name.png')",
-      }
+        "background-landing": "url('../assets/images/background-landing.png')",
+        logo: "url('../assets/images/logo-only.png')",
+        "logo-name": "url('../assets/images/logo-name.png')",
+      },
     },
   },
   plugins: [],
-
-}
+};
