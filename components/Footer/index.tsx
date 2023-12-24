d865a33d31667739b816a8c90853e5a02a738102
Murilo Gois <murilogois59@gmail.com>
2023-12-24 02:27:17 -0300

    feat(Footer): Add component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col flex-wrap mx-auto max-w-7xl justify-center items-center p-10">
+      <p className="text-center">
+        Copyright &copy; 2032 goisMurilo | All rights reserved.
+      </p>
+      <a
+        href="https://www.linkedin.com/in/goismurilo/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-white hover:text-primary-light flex items-center"
+      >
+        <p>Desenvolvido por </p>
+        <span>
+          <Image
+            src="/logo-with-name.svg"
+            alt="goisMurilo Logo"
+            width={100}
+            height={80}
+          />
+        </span>
+      </a>
+    </footer>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Projects from "../components/Projects";
 import Experience from "../components/Experience";
 import Contact from "../components/Contact";
 import AboutMe from "../components/AboutMe";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
@@ -23,30 +24,10 @@ const Home: NextPage = () => {
       {/* //TODO
       <AddExperinceInNumvbers /> */}
       <Projects />
-      <Experience />
-      <Contact />
+      {/* <Experience /> */}
+      {/* <Contact /> */}
 
-      <footer className="flex flex-wrap justify-center items-center p-10">
-        <a
-          href="https://www.linkedin.com/in/goismurilo/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white hover:text-primary-light flex items-center"
-        >
-          <p>Desenvolvido por </p>
-          <span>
-            <Image
-              src="/logo-with-name.svg"
-              alt="goisMurilo Logo"
-              width={200}
-              height={80}
-            />
-          </span>
-        </a>
-        <p className="text-white w-screen text-center">
-          Copyright &copy; 2032 goisMurilo | All rights reserved.
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 };
